de1d48ac9a915be5c5a89a55011ae9283f53e78f
Unknown <matt.j.niehoff@gmail.com>
2018-05-06 15:47:01 -0500

    Creates basic grid recycler view.

--- app/src/main/java/com/mattniehoff/tmdbandroidviewer/MainActivity.java
+++ app/src/main/java/com/mattniehoff/tmdbandroidviewer/MainActivity.java
@@ -2,23 +2,37 @@ package com.mattniehoff.tmdbandroidviewer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView helloWorldTextView;
+    private RecyclerView recyclerView;
+    private MovieAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private String[] testData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        helloWorldTextView = (TextView) findViewById(R.id.helloworld_tv);
-        String apiKey = ApiKeys.TmdbApiKey;
-        if (apiKey != null && !apiKey.isEmpty()) {
-            helloWorldTextView.setText(apiKey);
-        } else {
-            helloWorldTextView.setText("Failed to load API Key.");
-        }
+        recyclerView = (RecyclerView) findViewById(R.id.movies_recycler_view);
+
+        recyclerView.setHasFixedSize(true);
+
+        // Set layout manager
+        layoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(layoutManager);
+
+        populateMovieGrid();
+    }
+
+    private void populateMovieGrid() {
+        testData = new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+
+        adapter = new MovieAdapter(testData);
+        recyclerView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/mattniehoff/tmdbandroidviewer/MovieAdapter.java
@@ -0,0 +1,45 @@
+package com.mattniehoff.tmdbandroidviewer;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+    private String[] data;
+    private int numberOfDataItems;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView placeholderTextView;
+        public ViewHolder (View v) {
+            super (v);
+            placeholderTextView = (TextView) v.findViewById(R.id.placeholder_text_view);
+        }
+    }
+
+    public MovieAdapter(String [] data){
+        this.data = data;
+
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.grid_block, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.placeholderTextView.setText(data[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.length;
+    }
+}
