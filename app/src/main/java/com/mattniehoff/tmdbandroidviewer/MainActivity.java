fd1ec1e79dab006dcb268edfce75544eb02ff200
Unknown <matt.j.niehoff@gmail.com>
2018-05-06 13:51:26 -0500

    Adds apikeys template class.

--- app/src/main/java/com/mattniehoff/tmdbandroidviewer/MainActivity.java
+++ app/src/main/java/com/mattniehoff/tmdbandroidviewer/MainActivity.java
@@ -2,12 +2,23 @@ package com.mattniehoff.tmdbandroidviewer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView helloWorldTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        helloWorldTextView = (TextView) findViewById(R.id.helloworld_tv);
+        String apiKey = ApiKeys.TmdbApiKey;
+        if (apiKey != null && !apiKey.isEmpty()) {
+            helloWorldTextView.setText(apiKey);
+        } else {
+            helloWorldTextView.setText("Failed to load API Key.");
+        }
     }
 }
